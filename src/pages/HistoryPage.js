17049e7d3212d40132e431360b5e1554ff02ede2
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-08 10:53:30 -0600

    HistoryPage Used bootstrap for styling and responsiveness. Used the Stack Layout.

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -1,25 +1,26 @@
 import React from "react";
+import Stack from "react-bootstrap/Stack";
 
 function HistoryPage() {
+  const stanLeeImageUrl =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Stan_Lee_by_Gage_Skidmore_3.jpg/800px-Stan_Lee_by_Gage_Skidmore_3.jpg";
 
-    const stanLeeImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Stan_Lee_by_Gage_Skidmore_3.jpg/800px-Stan_Lee_by_Gage_Skidmore_3.jpg';
-  
-    // Other information about Stan Lee you may want to display
-    const stanLeeInfo = {
-      name: 'Stan Lee',
-      lifespan: '1922–2018',
-      quote: 'With great power comes great responsibility.'
-    };
-  
-    return (
-      <div>
-        <h1>{stanLeeInfo.name}</h1>
-        <img src={stanLeeImageUrl} alt="Stan Lee" />
-        <p>Lived: {stanLeeInfo.lifespan}</p>
-        <blockquote>{`"${stanLeeInfo.quote}"`}</blockquote>
-        {/* Additional content about the creation of Spider-Man */}
-      </div>
-    );
+  // Other information about Stan Lee you may want to display
+  const stanLeeInfo = {
+    name: "Stan Lee",
+    lifespan: "1922–2018",
+    quote: "With great power comes great responsibility.",
   };
+
+  return (
+    <Stack gap={3}>
+      <h1 className="p-2">{stanLeeInfo.name}</h1>
+      <img className="p-2" src={stanLeeImageUrl} alt="Stan Lee" />
+      <p className="p-2">Lived: {stanLeeInfo.lifespan}</p>
+      <blockquote className="p-2">{`"${stanLeeInfo.quote}"`}</blockquote>
+      {/* Additional content about the creation of Spider-Man */}
+    </Stack>
+  );
+}
 
 export default HistoryPage;
