5ef5266d4bd0e2e2f09e2ff01533e10622aecfe5
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-08 11:14:37 -0600

    HistoryPage Added a bio section and added the data into the display. Next up I'll make an object for Spider-Mans info and the display it as well. Also I will look into make the info collapsable and adding links at the top of the page for navigating to the history you want to view.

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -9,15 +9,17 @@ function HistoryPage() {
   const stanLeeInfo = {
     name: "Stan Lee",
     lifespan: "1922–2018",
-    quote: "With great power comes great responsibility.",
+    quote: "Excelsior!",
+    bio: "Stan Lee, born Stanley Martin Lieber on December 28, 1922, was an iconic figure in the comic book world, renowned for his pivotal role in the creation and development of Marvel Comics. His career in comics began in the early 1940s, and through the years he co-created numerous beloved characters, including Spider-Man, the X-Men, Iron Man, Thor, the Hulk, the Fantastic Four, Black Widow, Daredevil, and Doctor Strange. Lee's approach to superheroes was groundbreaking; he imbued his characters with complex personalities, flaws, and relatable struggles, diverging from the archetypical paragons of virtue that dominated the genre before. This humanized his superheroes and made Marvel Comics particularly resonant with readers of all ages, fostering a deeper connection to the characters and their stories.",
   };
 
   return (
     <Stack gap={3}>
-      <h1 className="p-2">{stanLeeInfo.name}</h1>
-      <img className="p-2" src={stanLeeImageUrl} alt="Stan Lee" />
+      <h1 className="p-1">{stanLeeInfo.name}</h1>
+      <img className="p-1" src={stanLeeImageUrl} alt="Stan Lee" />
       <p className="p-2">Lived: {stanLeeInfo.lifespan}</p>
       <blockquote className="p-2">{`"${stanLeeInfo.quote}"`}</blockquote>
+      <p className="p-2">{stanLeeInfo.bio}</p>
       {/* Additional content about the creation of Spider-Man */}
     </Stack>
   );
