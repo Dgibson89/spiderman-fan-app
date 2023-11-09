ca2c1bb820bdce66c955496fbc4806be443fd466
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-09 15:34:14 -0600

    HeroSection I added text shadow through css that dynamically changes with the theme.

--- src/components/common/HeroSection.js
+++ src/components/common/HeroSection.js
@@ -1,18 +1,21 @@
 import React from "react";
 import classicSuitImage from "../../assets/images/classicSuit.jpg";
 import symbioteSuitImage from "../../assets/images/symbioteSuit.jpg";
-import "../../styles/HeroSection.css"; 
+import "../../styles/HeroSection.css";
 function HeroSection({ theme }) {
-  const imageUrl =
-    theme === "light"
-      ? classicSuitImage 
-      : symbioteSuitImage; 
+  const imageUrl = theme === "light" ? classicSuitImage : symbioteSuitImage;
+
+  const textShadowClass =
+    theme === "light" ? "text-shadow-light" : "text-shadow-dark";
+
   return (
     <div
       className={`hero-section ${theme}`}
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-    {/* <p>Welcome to the Spider-Verse!</p> */}
+      <h1 className={textShadowClass}>
+        Swing Into the Spider-Verse: Your Ultimate Portal to All Things Spidey!
+      </h1>
     </div>
   );
 }
