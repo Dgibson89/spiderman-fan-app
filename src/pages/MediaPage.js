58bd2a7dbf5a0f40f416c45549fcb6e03eb28aeb
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-11 00:59:51 -0600

    MediaPage Added a youtube video that a coworker/fellow web head made a few years ago.

--- src/pages/MediaPage.js
+++ src/pages/MediaPage.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Container } from "react-bootstrap";
 
-function MediaPage() {
-    const constructionImageUrl = "https://www.seekpng.com/png/detail/259-2598162_page-in-under-construction.png"
-    return (
-      <div>
-         <Container>
-        <img
-          className="img-fluid p-1"
-          src={constructionImageUrl}
-          alt="coming soon!"
-        />
-      </Container>
+const MediaPage = () => {
+  // Example YouTube video embed URL
+  const videoEmbedUrl = "https://www.youtube-nocookie.com/embed/2T4pOB7d8xk?si=43YqW0gSImk922w7";
+
+  return (
+    <Container className="my-4">
+      <div className="ratio ratio-16x9">
+        <iframe width="560" height="315" src={videoEmbedUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
-    )
-}
+    </Container>
+  );
+};
 
-export default MediaPage
+export default MediaPage;
