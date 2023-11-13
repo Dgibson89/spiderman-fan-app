3eb603f7e068b71b939c7103f4bc130164ea514a
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-12 23:49:27 -0600

    Everything Works! Sort of. I had issues with errors but I kind of expected them. I can use methods to map through items that are not providing the same content, so I had to add empty arrays in the characterData to prevent errors. My HistoryPage is displaying again! The styling is off now, I will fix that next.

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -1,133 +1,25 @@
-import React, { useContext } from "react";
-import ThemeContext from "../utils/ThemeContext";
-import { Accordion, AccordionItem, Container } from "react-bootstrap";
-import "../styles/HistoryPage.css";
-import Stack from "react-bootstrap/Stack";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React, { useContext } from 'react';
+import { Accordion } from 'react-bootstrap';
+import AccordionItem from '../components/common/AccordionItem'; // Adjust the path as necessary
+import ThemeContext from '../utils/ThemeContext'; // Adjust the path as necessary
+import characterData from '../data/characterData'; // Adjust the path as necessary
 
-function HistoryPage() {
+const HistoryPage = () => {
   const { theme } = useContext(ThemeContext);
-
-
-  const accordionClassName =
-    theme === "light" ? "accordion-light" : "accordion-dark";
+  const accordionClassName = theme === 'light' ? 'accordion-light' : 'accordion-dark';
 
   return (
-    <>
-      <Container id="padding" data-bs-theme={theme === "light" ? "light" : "dark"}>
-        <Row className="justify-content-center">
-          <Col xs={12} md={8} lg={6}>
-            <Stack gap={3}>
-              <Accordion className={accordionClassName}>
-                <AccordionItem>
-                  <Accordion.Header>
-                    <h1 className="p-1">{stanLeeInfo.name}</h1>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    <img
-                      className="img-fluid p-1"
-                      src={stanLeeImageUrl}
-                      alt="Stan Lee"
-                    />
-                    <p className="p-2">Lived: {stanLeeInfo.lifespan}</p>
-                    <blockquote className="p-2">{`"${stanLeeInfo.quote}"`}</blockquote>
-                    <p className="p-2">Bio: {stanLeeInfo.bio}</p>
-                  </Accordion.Body>
-                </AccordionItem>
-              </Accordion>
-            </Stack>
-          </Col>
-        </Row>
-      </Container>
-
-      <Container id="padding" data-bs-theme={theme === "light" ? "light" : "dark"}>
-        <Row className="justify-content-center">
-          <Col xs={12} md={8} lg={6}>
-            <Stack gap={3}>
-              <Accordion className={accordionClassName} defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                  <Accordion.Header>
-                    <h1 className="p-1">{spiderManInfo.name}</h1>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    <img
-                      className="img-fluid p-1"
-                      src={spiderManImageUrl}
-                      alt="Spider-Man"
-                    />
-                    <p className="p-2">
-                      Powers: {spiderManInfo.powers.join(", ")}{" "}
-                    </p>
-                    <div className="p-2">
-                      <strong>Enemies:</strong>
-                      <ul>
-                        {spiderManInfo.enemies.map((enemy, index) => (
-                          <li key={index}>
-                            <a
-                              href={enemy.url}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              {enemy.name}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                    <p className="p-2">Bio: {spiderManInfo.bio}</p>
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
-            </Stack>
-          </Col>
-        </Row>
-      </Container>
-
-      <Container id="padding" data-bs-theme={theme === "light" ? "light" : "dark"}>
-        <Row className="justify-content-center">
-          <Col xs={12} md={8} lg={6}>
-            <Stack gap={3}>
-              <Accordion className={accordionClassName} defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                  <Accordion.Header>
-                    <h1 className="p-1">{spiderSymbioteInfo.name}</h1>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    <img
-                      className="img-fluid p-1"
-                      src={spiderSymbioteImageUrl}
-                      alt="Spider-Man (Symbiote)"
-                    />
-                    <p className="p-2">
-                      Powers: {spiderSymbioteInfo.powers.join(", ")}{" "}
-                    </p>
-                    <div className="p-2">
-                      <strong>Enemies:</strong>
-                      <ul>
-                        {spiderSymbioteInfo.enemies.map((enemy, index) => (
-                          <li key={index}>
-                            <a
-                              href={enemy.url}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              {enemy.name}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                    <p className="p-2">Bio: {spiderSymbioteInfo.bio}</p>
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
-            </Stack>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Accordion defaultActiveKey="0">
+      {characterData.map((character, index) => (
+        <AccordionItem 
+          key={index} // It's better if you have a unique id for each character to use here instead of index
+          info={character}
+          imageUrl={character.imageUrl}
+          accordionClassName={accordionClassName}
+        />
+      ))}
+    </Accordion>
   );
-}
+};
 
 export default HistoryPage;
