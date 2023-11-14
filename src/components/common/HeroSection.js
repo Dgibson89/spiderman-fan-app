27ade9713815df22c2813540feddee0c134edded
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-14 12:12:03 -0600

    HeroSection Just a space added after tge last import.

--- src/components/common/HeroSection.js
+++ src/components/common/HeroSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import classicSuitImage from "../../assets/images/classicSuit.jpg";
 import symbioteSuitImage from "../../assets/images/symbioteSuit.jpg";
 import "../../styles/HeroSection.css";
+
 function HeroSection({ theme }) {
   const imageUrl = theme === "light" ? classicSuitImage : symbioteSuitImage;
 
