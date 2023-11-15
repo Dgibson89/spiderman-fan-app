58e1a290e9044def0718b909c10b0340696ec646
Dgibson89 <donovan.gibson89@gmail.com>
2023-11-14 22:20:34 -0600

    Modal Control I've added session storage and state for isMinimized to control whether the modal stays minimized once someone navigates to a new page and then returns to the HomePage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import HeroSection from "../components/common/HeroSection";
 import SignupModal from "../components/common/SignupModal";
@@ -7,23 +7,33 @@ import ThemeContext from "../utils/ThemeContext";
 
 function HomePage() {
   const { theme } = useContext(ThemeContext); // Use the context to get the theme
-  const [showModal, setShowModal] = useState(true);
-  const [isMinimized, setIsMinimized] = useState(false);
+  const [showModal, setShowModal] = useState(() => !sessionStorage.getItem('modalShown'));
+  const [isMinimized, setIsMinimized] = useState(() => sessionStorage.getItem('modalMinimized') === 'true');
+
+  useEffect(() => {
+    if (showModal) {
+      sessionStorage.setItem('modalShown', 'true');
+    }
+    sessionStorage.setItem('modalMinimized', isMinimized.toString());
+  }, [showModal, isMinimized]);
 
   const closeModal = () => {
     setShowModal(false);
     setIsMinimized(true);
+    sessionStorage.setItem('modalMinimized', 'true');
   };
 
   const openModal = () => {
     setShowModal(true);
     setIsMinimized(false);
+    sessionStorage.setItem('modalMinimized', 'false');
   };
+
   return (
     <div>
       <HeroSection theme={theme} />
       {showModal && <SignupModal showModal={showModal} closeModal={closeModal} />}
-      {isMinimized && (
+      {!showModal && isMinimized && (
         theme === 'dark' ? 
           <Button className="btn-placement" variant="dark" onClick={openModal}>Open Sign Up</Button> :
           <Button className="custom-light-button btn-placement" onClick={openModal}>Open Sign Up</Button>
